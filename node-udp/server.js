a1247ee85b39c4ae5de5ae36da3d604ad853b12a
cadico <ricardo.rolo.almeida@gmail.com>
2022-05-12 23:45:39 -0300

    add json msg e handler no server

--- node-udp/server.js
+++ node-udp/server.js
@@ -12,15 +12,23 @@ server.on('listening', function () {
 
 //================ When receiving data from client 
 server.on('message', function (msg, info) {
-    // msg = decodeURIComponent(msg)
-    msg = JSON.parse(msg.toJSON())
-    console.log(msg)
-    if (Number.isInteger(msg.val) && msg.type === 'int') {
-        console.log('Data received from client : ' + msg.toString());
-        console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
-        var response = Buffer.from({ 'type': 'int', val: msg.val + 1 }).toJSON();
-    }
+    let buffer_string = Buffer.from(msg).toString()
+    console.log("Message from client: ", buffer_string);
+    var teste = parseInt(buffer_string)
+    var response = Buffer.from(`${teste + 1}`)
+    var teste = JSON.parse(buffer_string)
+    console.log(teste.type);
+    // var response = Buffer.from(`{"type": "int", "val": "${teste + 1}""`)
+    // const m = Buffer.from(msg, 'utf8')
+    // console.log(m['val'])
+    // if (Number.isInteger(msg.val) && msg.type === 'int') {
+    //     console.log('Data received from client : ' + msg.toString());
+    //     console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
+    // }
     //sending msg to the client
+    // `teste: ${var}`
+    // Buffer.from(`{type: 'int', 'val': ${msg.val+1}}`);
+
     server.send(response, info.port, 'localhost', function (error) {
         if (error) {
             client.close();
--- python-udp/python-client.py
+++ python-udp/python-client.py
@@ -1,9 +1,12 @@
 import json
 import socket
 
-msgFromClient = {'type': 'int', 'val': 100 }
+# msgFromClient = 100
 
-bytesToSend = str(msgFromClient).encode('utf8')
+msgFromClient = {"type": "int", "val": 100}
+bytesToSend = json.dumps(msgFromClient).encode("utf-8")
+
+# bytesToSend = str(msgFromClient).encode('utf8')
 
 serverAddressPort = ("127.0.0.1", 7788)
 
@@ -15,10 +18,19 @@ UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Send to server using created UDP socket
 
+# HOST = "127.0.0.1"  # The server's hostname or IP address
+# PORT = 7788  # The port used by the server
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     msgFromClient = {"type": "int", "val": 100}
+#     s.connect((HOST, PORT))
+#     s.sendall(b"Hello World")
+#     data = s.recv(1024)
+
 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
 msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
 msg = "Message from Server {}".format(msgFromServer[0])
-
 print(msg)
+# print(f"Received {data!r}")
